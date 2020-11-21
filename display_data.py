c18e6f488e191f66dd391201231032fce0e3c451
Rohan Dugad <rohand2468@gmail.com>
2020-11-20 22:12:39 -0500

    Merge in all commits from modules-keyboard-class except 2bcbef26 which deleted all code from create-read-data-script

--- display_data.py
+++ display_data.py
@@ -1,17 +1,27 @@
 #!usr/bin/env python3
 """This script will display the data in the form of a slideshow"""
 
+import cv2
+import modules
 from display_data_utils import DisplayData
 
 def main():
     """Main code"""
+    img_dir = modules.DirectoryManagement.ReadDir(target_dir=r'/home/pi/Documents/Images/Test8')
+    img_dir.read()
     keyboard = DisplayData.Keyboard()
     keyboard.start()
 
-    while True:
-        keyboard.consume()
+    try:
+        while True:
+            img_dir.imshow()
+            img_dir.update(250)
 
-    keyboard.stop()
+            keyboard.consume()
+
+    except modules.Break:
+        cv2.destroyAllWindows()
+        keyboard.stop()
 
 if __name__ == '__main__':
     main()
