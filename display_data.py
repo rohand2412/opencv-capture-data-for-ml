a92a0c8ba369dff2dd4927617b73151f64efccd5
Rohan Dugad <rohand2468@gmail.com>
2020-11-23 23:13:50 -0500

    Cleared display_data.py to be a sandbox for unit testing timer class

--- display_data.py
+++ display_data.py
@@ -5,21 +5,7 @@ import cv2
 from display_data_utils import DisplayData
 
 def main():
-    """Main code"""
-    img_dir = DisplayData.DirectoryManagement.ReadDir(target_dir=r'/home/pi/Documents/Images/Test8',
-                                                      mode=DisplayData.READDIR_SLIDESHOW_MODE_KEYBOARD)
-    img_dir.read()
 
-    try:
-        while True:
-            img_dir.imshow()
-            img_dir.update()
-
-            DisplayData.check_for_quit_request()
-
-    except DisplayData.Break:
-        cv2.destroyAllWindows()
-        img_dir.close()
 
 if __name__ == '__main__':
     main()
