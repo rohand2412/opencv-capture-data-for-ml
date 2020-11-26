f59042c2e454e03413aec442d6e30b02381f0562
Rohan Dugad <rohand2468@gmail.com>
2020-11-25 22:13:27 -0500

    Implemented single --data-dir arguement for display_data.py using raw argparse library

--- display_data.py
+++ display_data.py
@@ -1,11 +1,17 @@
 #!usr/bin/env python3
 """This script will display the data in the form of a slideshow"""
 
+import argparse
 import cv2
 from display_data_utils import DisplayData
 
 def main():
-
+    """Main code"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", default="Test0", type=str,
+                        help="directory from which data will be displayed")
+    args = parser.parse_args()
+    print(args.data_dir)
 
 if __name__ == '__main__':
     main()
