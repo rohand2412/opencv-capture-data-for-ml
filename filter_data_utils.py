e198c53063c11c93b1e9a994b2a3cc61fb292799
Rohan Dugad <rohand2468@gmail.com>
2021-02-14 22:11:14 -0500

    Moved get_ordered_path to base_utils

--- filter_data_utils.py
+++ filter_data_utils.py
@@ -7,37 +7,6 @@ import base_utils
 
 class FilterData(base_utils.Packages):
     """Class that adapts parent's modules for FilterData"""
-    @staticmethod
-    def get_ordered_path(path):
-        """Returns list of all items in specified path in numerical order"""
-        files = os.listdir(path)
-        num_data_types = 3
-        files_split = [[None for data_type in range(num_data_types)] for name in range(len(files))]
-
-        for i, name in enumerate(files):
-            text = ''.join(filter(str.isalpha, os.path.splitext(name)[0]))
-            num = ''.join(filter(str.isdigit, name))
-            ext = os.path.splitext(name)[1]
-            files_split[i] = [text, num, ext]
-
-        files_split = np.array(files_split)
-        files_ordered = [None for name in range(len(files_split))]
-        files_ordered_len = 0
-        files_cur_index = 0
-        while files_ordered_len < len(files_ordered):
-            indices = np.where(files_split[:, 1]==str(files_cur_index))
-            if len(indices[0]) == 1:
-                files_ordered[files_ordered_len] = files_split[indices[0][0]][0] \
-                                                   + files_split[indices[0][0]][1] \
-                                                   + files_split[indices[0][0]][2]
-                files_ordered_len += 1
-            elif indices[0].size > 0:
-                while True:
-                    print("[ERROR] MULTIPLE DIRECTORIES WITH THE SAME ID")
-            files_cur_index += 1
-
-        return files_ordered
-
     class ArguementError(Exception):
         """Used to report errors from InitBashArgs class"""
 
