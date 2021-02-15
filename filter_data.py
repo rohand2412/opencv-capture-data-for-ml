845e8aa129be849ebaa9554943e03a5b0aa9b843
Rohan Dugad <rohand2468@gmail.com>
2021-02-14 19:36:07 -0500

    Adapted for command line use

--- filter_data.py
+++ filter_data.py
@@ -2,74 +2,40 @@
 """This script will filter the data by division and export it"""
 
 import os
-import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
+from filter_data_utils import FilterData
 
-data_path = '/home/rohan/Documents/RCJ2021Repos/raspberry-pi-images-rcj/Evac/'
-save_path = '/home/rohan/Documents/RCJ2021Repos/raspberry-pi-images-rcj/Evac-Final-Images/'
-os.chdir(data_path)
+def main():
+    """Main code"""
+    FilterData.InitBashArgs()
+    args = FilterData.InitBashArgs.get_args()
 
-def getOrderedPath(path):
-    files = os.listdir(path)
-    num_data_types = 3
-    files_split = [[None for data_type in range(num_data_types)] for name in range(len(files))]
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+    filter_factor = args.filter_factor
 
-    for i, name in enumerate(files):
-        text = ''.join(filter(str.isalpha, os.path.splitext(name)[0]))
-        num = ''.join(filter(str.isdigit, name))
-        ext = os.path.splitext(name)[1]
-        files_split[i] = [text, num, ext]
+    os.chdir(input_dir)
 
-    files_split = np.array(files_split)
-    files_ordered = [None for name in range(len(files_split))]
-    files_ordered_len = 0
-    files_cur_index = 0
-    while files_ordered_len < len(files_ordered):
-        indices = np.where(files_split[:, 1]==str(files_cur_index))
-        if len(indices[0]) == 1:
-            files_ordered[files_ordered_len] = files_split[indices[0][0]][0] + files_split[indices[0][0]][1] + files_split[indices[0][0]][2]
-            files_ordered_len += 1
-        elif indices[0].size > 0:
-            while true:
-                print("[ERROR] MULTIPLE DIRECTORIES WITH THE SAME ID")
-        files_cur_index += 1
-    
-    return files_ordered
+    file_num = 0
+    for folder in os.listdir():
+        file_num += len(os.listdir(folder))
+    file_num = file_num // filter_factor
 
-filter_factor = 5
+    file_id = 0
+    file_paths = [None for filename in range(file_num)]
+    for folder in FilterData.get_ordered_path(os.getcwd()):
+        for filename in FilterData.get_ordered_path(folder):
+            if not file_id % filter_factor:
+                file_paths[file_id // filter_factor] = folder + '/' + filename
+            file_id += 1
 
-img_num = 0
+    file_format = "RGB"
+    for filename in range(file_num):
+        if file_paths[filename]:
+            data = Image.open(file_paths[filename]).convert(file_format)
+            text = ''.join(filter(str.isalpha, os.path.splitext(os.path.basename(file_paths[filename]))[0]))
+            ext = os.path.splitext(os.path.basename(file_paths[filename]))[1]
+            data.save(output_dir + text + str(filename) + ext)
 
-for folder in os.listdir():
-    for img in os.listdir(folder):
-        img_num += 1
-
-img_num = img_num // filter_factor
-
-img_paths = [None for img in range(img_num)]
-
-img_id = 0
-
-for folder in getOrderedPath(os.getcwd()):
-    for img in getOrderedPath(folder):
-        if not img_id % filter_factor:
-            img_paths[img_id // filter_factor] = folder + "/" + img
-        img_id += 1
-
-display_photo = np.array(Image.open(img_paths[-1]))
-
-fig, ax = plt.subplots(1)
-
-ax.imshow(display_photo)
-
-plt.show()
-
-img_format = "RGB"
-
-for img in range(img_num):
-    if img_paths[img]:
-        image = Image.open(img_paths[img]).convert(img_format)
-        text = ''.join(filter(str.isalpha, os.path.splitext(os.path.basename(img_paths[img]))[0]))
-        ext = os.path.splitext(os.path.basename(img_paths[img]))[1]
-        image.save(save_path + text + str(img) + ext)
+if __name__ == '__main__':
+    main()
