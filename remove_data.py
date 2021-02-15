1f2d49bce08db1f909090163056bc1bac3d33970
Rohan Dugad <rohand2468@gmail.com>
2021-02-15 16:34:43 -0500

    Implemented saving bad imgs to trash dir

--- remove_data.py
+++ remove_data.py
@@ -12,6 +12,7 @@ def main():
 
     input_dir = args.input_dir
     output_dir = args.output_dir
+    trash_dir = args.trash_dir
     remove_ids = args.remove_ids
 
     os.chdir(input_dir)
@@ -21,13 +22,15 @@ def main():
 
     for filename in RemoveData.Dataset.get_ordered_path(os.getcwd()):
         file_num = int(''.join(filter(str.isdigit, filename)))
+        data = Image.open(filename).convert(img_format)
+        text = ''.join(filter(str.isalpha, os.path.splitext(filename)[0]))
+        ext = os.path.splitext(filename)[1]
 
         if file_num not in remove_ids:
-            data = Image.open(filename).convert(img_format)
-            text = ''.join(filter(str.isalpha, os.path.splitext(filename)[0]))
-            ext = os.path.splitext(filename)[1]
             data.save(output_dir + text + str(file_id) + ext)
             file_id += 1
+        elif file_num in remove_ids:
+            data.save(trash_dir + text + str(file_num) + ext)
 
 if __name__ == '__main__':
     main()
