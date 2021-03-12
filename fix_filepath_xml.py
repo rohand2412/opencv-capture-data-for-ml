d353879ca18b314f6ffab7486e6cdf37ea9b68ea
Rohan Dugad <rohand2468@gmail.com>
2021-03-12 00:33:43 -0500

    Added fixing code

---
+++ fix_filepath_xml.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""This script will fix the filepaths in the specified xmls to be their current file location"""
+
+import os
+import xml.etree.ElementTree as ET
+from fix_filepath_xml_utils import FixFilepathXML
+
+def main():
+    """Main code"""
+    FixFilepathXML.InitBashArgs()
+    args = FixFilepathXML.InitBashArgs.get_args()
+
+    xml_dir = args.xml_dir
+    img_dir = args.images_dir
+    img_ext = os.path.splitext(os.listdir(img_dir)[0])[1]
+
+    for filename in FixFilepathXML.Dataset.get_ordered_path(xml_dir):
+        text = os.path.splitext(filename)[0]
+
+        mytree = ET.parse(xml_dir + filename)
+        myroot = mytree.getroot()
+
+        for path in myroot.iter('path'):
+            path.text = img_dir + text + img_ext
+
+        mytree.write(xml_dir + filename)
+
+if __name__ == '__main__':
+    main()
