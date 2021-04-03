73e2859c146bdd31ce84ed46546f513d63eb239c
Rohan Dugad <rohand2468@gmail.com>
2021-04-02 21:41:30 -0400

    Implemented use of label map

--- display_objects.py
+++ display_objects.py
@@ -51,7 +51,7 @@ def main():
             scores  = interpreter.get_tensor(output_details[2]['index'])
             num     = interpreter.get_tensor(output_details[3]['index'])
 
-            labels = ["zone"]
+            labels = DisplayObjects.Dataset.load_label_map(labels_path)
 
             for i in range(int(num[0])):
                 if (((boxes[0][i][3] - boxes[0][i][1]) / (boxes[0][i][2] - boxes[0][i][0])) > 1.5) and scores[0][i] > 0.25:
