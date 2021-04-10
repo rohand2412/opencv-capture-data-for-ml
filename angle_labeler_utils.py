a1df19a3538edf13b50ff4b018d1c1e77f4f044e
Rohan Dugad <rohand2468@gmail.com>
2021-04-10 01:19:30 -0400

    Created bash args

---
+++ angle_labeler_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This script contains all of the modules used in angle_labeler.py"""
+
+from raspberry_pi_libraries import multi_wrapper
+
+class AngleLabeler(multi_wrapper.Packages):
+    """Class that adapts parent's modules for AngleLabeler"""
+    class InitBashArgs(multi_wrapper.Packages.InitBashArgs):
+        """Initalizes the arguements present for bash execution which will be different for each
+        application of this wrapper """
+        @classmethod
+        def get_arg_params(cls):
+            """Returns the argument paramters"""
+            cls._parser.add_argument("--labels-path", default=None, type=str,
+                                     help="path to labels.csv")
+            cls._parser.add_argument("--images-dir", default=None, type=str,
+                                     help="directory with images to label")
+
+        @classmethod
+        def get_args(cls):
+            """Returns data inputted from bash"""
+            if not cls._args.labels_path:
+                raise multi_wrapper.Packages.ArguementError(f"No input directory specified." \
+                                                             + " Please specify '--labels-path'" \
+                                                             + "arguement")
+            if not cls._args.images_dir:
+                raise multi_wrapper.Packages.ArguementError(f"No input directory specified." \
+                                                             + " Please specify '--images-dir'" \
+                                                             + "arguement")
+            return cls._args
