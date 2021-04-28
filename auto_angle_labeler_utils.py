8350a18246bc48fd398ae949b4054e53eafce390
Rohan Dugad <rohand2468@gmail.com>
2021-04-28 02:45:37 -0400

    Added bash args

---
+++ auto_angle_labeler_utils.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""This script contains all of the modules used in auto_angle_labeler.py"""
+
+from raspberry_pi_libraries import multi_wrapper
+
+class AutoAngleLabeler(multi_wrapper.Packages):
+    """Class that adapts parent's modules for AutoAngleLabeler"""
+    class InitBashArgs(multi_wrapper.Packages.InitBashArgs):
+        """Initalizes the arguements present for bash execution which will be different for each
+        application of this wrapper """
+        @classmethod
+        def get_arg_params(cls):
+            """Returns the argument paramters"""
+            cls._parser.add_argument("--model-path", default=None, type=str,
+                                     help="path to tflite model")
+            cls._parser.add_argument("--label-path", default=None, type=str,
+                                     help="path to labels.csv")
+            cls._parser.add_argument("--images-dir", default=None, type=str,
+                                     help="directory with images to label")
+
+        @classmethod
+        def get_args(cls):
+            """Returns data inputted from bash"""
+            if not cls._args.model_path:
+                raise multi_wrapper.Packages.ArguementError(f"No input directory specified." \
+                                                             + " Please specify '--model-path'" \
+                                                             + "arguement")
+            if not cls._args.label_path:
+                raise multi_wrapper.Packages.ArguementError(f"No input directory specified." \
+                                                             + " Please specify '--label-path'" \
+                                                             + "arguement")
+            if not cls._args.images_dir:
+                raise multi_wrapper.Packages.ArguementError(f"No input directory specified." \
+                                                             + " Please specify '--images-dir'" \
+                                                             + "arguement")
+            return cls._args
