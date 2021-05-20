3ab5fa5e55f17d20bf39b90ca26f4df4c58da3b8
Rohan Dugad <rohand2468@gmail.com>
2021-05-20 05:11:49 -0400

    Added bash args

---
+++ resize_data_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This script contains all of the modules used in resize_data.py"""
+
+from raspberry_pi_libraries import multi_wrapper
+
+class ResizeData(multi_wrapper.Packages):
+    """Class that adapts parent's modules for ResizeData"""
+    class InitBashArgs(multi_wrapper.Packages.InitBashArgs):
+        """Initalizes the arguements present for bash execution which will be different for each
+        application of this wrapper """
+        @classmethod
+        def get_arg_params(cls):
+            """Returns the argument paramters"""
+            cls._parser.add_argument("--input-dir", default=None, type=str,
+                                     help="directory from which data will be resized")
+            cls._parser.add_argument("--output-dir", default=None, type=str,
+                                     help="directory at which resized data will be saved")
+            cls._parser.add_argument("--new-size", default=300, type=int,
+                                     help="side length to be resized to")
+
+        @classmethod
+        def get_args(cls):
+            """Returns data inputted from bash"""
+            if not cls._args.input_dir:
+                raise multi_wrapper.Packages.ArguementError(f"No input directory specified. Please" \
+                                                             + " specify '--input-dir' arguement")
+            if not cls._args.output_dir:
+                raise multi_wrapper.Packages.ArguementError(f"No output directory specified. Please" \
+                                                             + " specify '--output-dir' argument")
+            return cls._args
